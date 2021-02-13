6781b022c982d57d299d28a341900c9c034eb237
tomas <tomas@dataclasson.se>
2021-02-13 19:14:15 +0100

    updated deploycss-2021.sh

--- deploycss-2021.sh
+++ deploycss-2021.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -rv huddingefriidrottsskola.se/css tomas@wordpress.daca.ovh:www/css
+rsync -rv huddingefriidrottsskola.se/css tomas@wordpress.daca.ovh:www/2021.huddingefriidrottsskola.se/css
